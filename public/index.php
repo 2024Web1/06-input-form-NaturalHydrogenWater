ea5b1fdeb379df4f76de326098f82a3af5603b47
NaturalHydrogenWater <kd1341034@st.kobedenshi.ac.jp>
2024-05-14 13:26:02 +0900

    授業

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="input.html">text形式の送信練習</a>
+<a href="radio.html">ラジオボタンの練習</a>
+<a href="checkbox.html">チェックボックスの練習</a>
+<a href="hidden.html">隠しフィールドの練習</a>
+
+</body>
+</html>
